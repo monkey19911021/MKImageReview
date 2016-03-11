7d735cd91ef86da43baa73126a4a0a662ab2a4df
liujh <457870936@qq.com>
2016-03-11 14:50:35 +0800

    Initial Commit

---
+++ MKImageReview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKImageReview
+//
+//  Created by Liujh on 16/3/11.
+//  Copyright © 2016年 com.minstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MKImageReview/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MKImageReview
+//
+//  Created by Liujh on 16/3/11.
+//  Copyright © 2016年 com.minstone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
