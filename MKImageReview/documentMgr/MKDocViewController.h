b5ac2232c3f49ab904f3cf4ef9fb1a90a1d15d00
jiahao <monkey19911021@gmail.com>
2016-09-19 17:31:25 +0800

    重构

---
+++ MKImageReview/documentMgr/MKDocViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKDocViewController.h
+//  MKImageReview
+//
+//  Created by DONLINKS on 16/9/19.
+//  Copyright © 2016年 com.minstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKDocViewController : UICollectionViewController
+
+@end
